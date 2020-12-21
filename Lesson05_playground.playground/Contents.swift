db840e9be218c6987edb4e829f810a4191fb2162
Paul Thomas <paulthomas2107@gmail.com>
2020-12-21 17:15:31 +0000

    more commit

---
+++ Lesson05_playground.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// This is obv a comment
+var str = "Hello, playground"
+
+str = "Hello, Paul"
+print(str)
+
+let paul = "Paul"
+print(paul)
+
+var b = true
+print(b)
+b = false
+print(b)
+print(b)
+
+var i  = 32
+i = 0
+i = -12
+print(i)
+
+var aFloat:Float = 12.23
+print(aFloat)
+var aDouble:Double = 1233003.3333
+print(aDouble)
+var anInt:Int = 12
+print(anInt)
+
+
+
+
