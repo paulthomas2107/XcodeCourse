b4901091a60284a53b54f030ff4aa96a1b225a0d
Paul Thomas <paulthomas2107@gmail.com>
2020-12-25 20:28:56 +0000

    App stuff

--- Lesson04/Lesson04/ViewController.swift
+++ Lesson04/Lesson04/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     
     @IBOutlet weak var leftImageView: UIImageView!
@@ -19,54 +19,81 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    @IBOutlet weak var leftRoundsScore: UILabel!
+    
+    @IBOutlet weak var rightRoundsScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-       
+        
     }
-
+    
     var scoreRight = 0
     var scoreLeft = 0
     let range: ClosedRange = 2...14
+    var leftRoundsWon = 0
+    var rightRoundsWon = 0
     
     @IBAction func dealTapped(_ sender: Any) {
         
-        let leftRandom = Int.random(in: range)
-        leftImageView.image = UIImage(named: "card\(leftRandom)")
-        
-        let rightRandom = Int.random(in: range)
-        rightImageView.image = UIImage(named: "card\(rightRandom)")
-        
-        if (leftRandom > rightRandom) {
-            scoreLeft += 1
-        } else if (rightRandom > leftRandom) {
-            scoreRight += 1
-        } else if (rightRandom == leftRandom){
-            scoreLeft += 1
-            scoreRight += 1
+            let leftRandom = Int.random(in: range)
+            leftImageView.image = UIImage(named: "card\(leftRandom)")
+            
+            let rightRandom = Int.random(in: range)
+            rightImageView.image = UIImage(named: "card\(rightRandom)")
+            
+            if (leftRandom > rightRandom) {
+                scoreLeft += 1
+            } else if (rightRandom > leftRandom) {
+                scoreRight += 1
+            } else if (rightRandom == leftRandom){
+                scoreLeft += 1
+                scoreRight += 1
+            }
+            
+            leftScoreLabel.text = String(scoreLeft)
+            rightScoreLabel.text = String(scoreRight)
+            
+            if (scoreRight == 20 || scoreLeft == 20) {
+                
+                if (scoreRight == 20) {
+                    rightRoundsWon += 1
+                    rightRoundsScore.text = String(rightRoundsWon)
+                } else {
+                    leftRoundsWon += 1
+                    leftRoundsScore.text = String(leftRoundsWon)
+                }
+                
+                /*
+                 let winner = scoreRight == 20 ? "CPU" : "Player"
+                 
+                 
+                 let alertController = UIAlertController(title: "War Winner!", message:
+                 winner, preferredStyle: .alert)
+                 alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                 self.present(alertController, animated: true, completion: nil)
+                 
+                 */
+                scoreRight = 0
+                scoreLeft = 0
+                
+               
+                
+            }
+            
         }
         
-        leftScoreLabel.text = String(scoreLeft)
-        rightScoreLabel.text = String(scoreRight)
+            
+            
+            
+            
         
-        if (scoreRight == 20 || scoreLeft == 20) {
-            
-            let winner = scoreRight == 20 ? "CPU" : "Player One"
-            
-            let alertController = UIAlertController(title: "War Winner!", message:
-                    winner, preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-                self.present(alertController, animated: true, completion: nil)
-            
-            scoreRight = 0
-            scoreLeft = 0
-            
-            
-        }
+        
         
     }
     
-}
+
 
 
